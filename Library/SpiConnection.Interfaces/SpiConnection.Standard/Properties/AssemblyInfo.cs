727ce7e238db00c7d8c1b9da64c25e043fdc068c
JosiCoder <JosiCoder@users.noreply.github.com>
2017-10-31 08:41:48 +0100

    AssemblyInfo unified

--- Library/SpiConnection.Interfaces/SpiConnection.Standard/Properties/AssemblyInfo.cs
+++ Library/SpiConnection.Interfaces/SpiConnection.Standard/Properties/AssemblyInfo.cs
@@ -1,27 +1,33 @@
-﻿using System.Reflection;
+﻿//------------------------------------------------------------------------------
+// Copyright (C) 2016 Josi Coder
+
+// This program is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+
+// This program is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+
+// You should have received a copy of the GNU General Public License along with
+// this program. If not, see <http://www.gnu.org/licenses/>.
+//--------------------------------------------------------------------------------
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using CtLab.Utilities;
 
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
+[assembly: AssemblyTitle(GlobalAssemblyInfo.AssemblyTitle)]
+[assembly: AssemblyDescription(GlobalAssemblyInfo.AssemblyDescription)]
+[assembly: AssemblyConfiguration(GlobalAssemblyInfo.AssemblyConfiguration)]
+[assembly: AssemblyCompany(GlobalAssemblyInfo.AssemblyCompany)]
+[assembly: AssemblyProduct(GlobalAssemblyInfo.AssemblyProduct)]
+[assembly: AssemblyCopyright(GlobalAssemblyInfo.AssemblyCopyright)]
+[assembly: AssemblyTrademark(GlobalAssemblyInfo.AssemblyTrademark)]
+[assembly: AssemblyCulture(GlobalAssemblyInfo.AssemblyCulture)]
 
-[assembly: AssemblyTitle ("SpiConnection.Standard")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("Josi")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion ("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
+[assembly: AssemblyVersion(GlobalAssemblyInfo.AssemblyVersion)]
+[assembly: AssemblyFileVersion(GlobalAssemblyInfo.AssemblyFileVersion)]
